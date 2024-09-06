99c55be490d8dbe45cbbbf02ea73daff0c4e7c23
DEL.NUL.NUL.SOH <youdontknowme3509@gmail.com>
2024-09-06 20:19:11 +0530

    ver 0.0.1
    +added console write function
    +transfer types to separate file
    TODO:
    +rigorous testing for current implementation
    + test TCP clients connection
    +add file writer for .txt

--- index.ts
+++ index.ts
@@ -1,54 +1,10 @@
+import {Level,Options,LevelArgs,tagArgs,tag,LogArgs} from './types.js';
+
 import chalk from 'chalk';
 import showdown from 'showdown';
-
 import { promises as fs } from 'fs';
-
-import { tcp } from './tcpclient';
-
-type tag={
-    tag:Number,
-    tagName:String,
-    tagMessage?:String,
-    tagcolor:String,
-    isDyanmic:Boolean
-}
-
-
-type Level = {
-    lvlName: String,
-    lvlcolor: String,
-    level:Number,
-    tags:tag[],
-    tagsCount:0
-};
-
-type LevelArgs={
-    lvlName: String,
-    lvlcolor?: String,
-    level?:Number,
-    tags?:tagArgs[]
-}
-
-type tagArgs={
-    tag?:Number,
-    tagName:String,
-    tagMessage?:String,
-    tagcolor?:String,
-}
-
-type Options={
-    tcpConnections?:{
-        adderess:string,
-        port:number
-        secretKey:string
-    }|{
-        adderess:string,
-        port:number
-        secretKey:string
-    }[],
-    files?:string|string[],
-    writeOnconsole?:boolean
-}
+import { tcp } from './tcpclient.js';
+import { getRandomValues } from 'crypto';
 
 export class lid{
     private levels:Level[];
@@ -56,11 +12,8 @@ export class lid{
     private options:Options;
     private tcpConnections;
     private files;
-    constructor(level:LevelArgs[],options:Options,SecretKey:string){
-
-        function getRandomNumber(min:number=0, max:number=360) {
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-        }
+    constructor(level:LevelArgs[],options:Options={},SecretKey:string=""){
+        
         function isIterable(obj: any): boolean {
             return obj != null && typeof obj[Symbol.iterator] === 'function';
         }
@@ -71,7 +24,7 @@ export class lid{
             const level:Level={
                 level:i.level??this.LevelCount,
                 lvlName:i.lvlName,
-                lvlcolor:this.hslToHex(getRandomNumber(),100,50),
+                lvlcolor:this.hslToHex(this.getRandomNumber(),100,50),
                 tags:[],
                 tagsCount:0
             }
@@ -79,9 +32,9 @@ export class lid{
             if(i.tags){
                 for(const j of i.tags){
                     level.tags.push({
-                        isDyanmic:false,
+                        isDynamic:false,
                         tag:j.tag??level.tagsCount,
-                        tagcolor:this.hslToHex(getRandomNumber(),100,50),
+                        tagcolor:this.hslToHex(this.getRandomNumber(),100,50),
                         tagMessage:j.tagMessage,
                         tagName:j.tagName
                     })
@@ -126,18 +79,104 @@ export class lid{
     addtag(){}
     removetag(){}
 
-    log(level:number,message:string,tag:tagArgs[],args:any={}){
+    log(level:number|string,message:string,args:LogArgs,tags:tagArgs[]|undefined=undefined){
+        let LoggingLevel:Level|undefined;
+            for (const levels of this.levels) {
+                if(typeof level==="number"){
+                    if(levels.level===level){
+                        LoggingLevel=levels;
+                    }
+                }else if(typeof level==="string"){
+                    if(levels.lvlName===level){
+                        LoggingLevel=levels;
+                }
+            }
+        }
+        if(!LoggingLevel){
+            console.error('Lid Logger error (id): cannot find level to log');
+            return;
+        }
+
+        let LoggingTags:tag[]=[];
+        if(tags){
+            if(LoggingLevel.tags && LoggingLevel.tagsCount===0){
+                for (const UserTags of tags) {
+                    LoggingTags.push({
+                        isDynamic:true,
+                        tag:LoggingLevel.tagsCount++,
+                        tagcolor:this.hslToHex(this.getRandomNumber(),100,50),
+                        tagName:UserTags.tagName,
+                        tagMessage:UserTags.tagMessage
+                    })
+                }
+                
+            }else{
+                
+
+                const copy = new Map(tags.map(tag => [tag.tagName, tag]));
+                
+                for (const Logtag of LoggingLevel.tags) {
+                    if (copy.has(Logtag.tagName)) {
+                        LoggingTags.push(Logtag);
+                        copy.delete(Logtag.tagName);
+                    }
+                }
+
+                for (const tag of copy.values()) {
+                    LoggingTags.push({
+                        isDynamic: true,
+                        tag: LoggingLevel.tagsCount++,
+                        tagcolor: this.hslToHex(this.getRandomNumber(), 100, 50),
+                        tagName: tag.tagName,
+                        tagMessage: tag.tagMessage
+                    });
+                }
+            
+            }
+        }
+
+        if(this.options.writeOnconsole){
+            this.writeconsole(LoggingLevel.lvlName,LoggingLevel.lvlcolor,message,LoggingTags);
+        }
+    }
+
+    writeconsole(lvlName:string,LvlColor:string,message:string,tags:tag[]=[]){
+        console.log(
+            new Date().toISOString(),
+            chalk.bgHex(LvlColor).black(lvlName.toUpperCase()),
+            chalk.hex(LvlColor)(message),
+        );
         
+        if(tags&&tags.length>0){
+            const tagtable:{}[]=[]
+            for (const tag of tags) {
+                tagtable.push({tags:chalk.bgHex(tag.tagcolor).black(tag.tagName),message:tag.tagMessage??''})
+            }
+            console.table(tagtable);
+        }
+    }
+
+    writefiles(){
+
+    }
+
+    writeConnection(){
+
+    }
+
+    private getRandomNumber(min:number=0, max:number=360) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
     private hslToHex(h:number, s:number, l:number) {
         l /= 100;
         const a = s * Math.min(l, 1 - l) / 100;
         const f = (n:number) => {
-          const k = (n + h / 30) % 12;
-          const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
-          return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
+            const k = (n + h / 30) % 12;
+            const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+            return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
         };
+
         return `#${f(0)}${f(8)}${f(4)}`;
       }
 
---
+++ types.ts
@@ -0,0 +1,48 @@
+export type tag = {
+    tag: number,
+    tagName: string,
+    tagMessage?: string,
+    tagcolor: string,
+    isDynamic: boolean
+};
+
+export type Level = {
+    lvlName: string,
+    lvlcolor: string,
+    level: number,
+    tags: tag[],
+    tagsCount: 0
+};
+
+export type LevelArgs = {
+    lvlName: string,
+    lvlcolor?: string,
+    level?: number,
+    tags?: tagArgs[]
+};
+
+export type tagArgs = {
+    tag?: number,
+    tagName: string,
+    tagMessage?: string,
+    tagcolor?: string,
+};
+
+export type Options = {
+    tcpConnections?: {
+        adderess: string,
+        port: number,
+        secretKey: string
+    } | {
+        adderess: string,
+        port: number,
+        secretKey: string
+    }[],
+    files?: string | string[],
+    writeOnconsole?: boolean
+};
+
+export type LogArgs={
+    excludeConnections?:string[]|string,
+    excludeFiles?:string[]|string
+}
